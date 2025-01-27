1fd9b45d842875e8a606a4022d18eeb98c550184
Nicolai Trandafil <nicktrandafil@gmail.com>
2025-01-27 02:06:05 +0200

    Test app

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,9 @@
 #include <QApplication>
+#include "form.h"
 
-int main(int argc, char*[] argv) {
+int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    Form form;
+    form.show();
     return app.exec();
 }
